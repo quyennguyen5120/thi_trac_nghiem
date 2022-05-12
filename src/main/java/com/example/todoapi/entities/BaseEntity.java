25f1848e09548d48f3f3eede6e30f5598b9777dd
vuivehieu <75554300+vuivehieu@users.noreply.github.com>
2022-05-12 10:05:24 +0700

    UpdateBaseEntity

--- src/main/java/com/example/todoapi/entities/BaseEntity.java
+++ src/main/java/com/example/todoapi/entities/BaseEntity.java
@@ -5,11 +5,15 @@ import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
 import java.sql.Timestamp;
 
 @Getter
 @Setter
+@MappedSuperclass
 public class BaseEntity {
+    @Column(updatable = false)
     @CreationTimestamp
     private Timestamp created_date;
     @UpdateTimestamp
