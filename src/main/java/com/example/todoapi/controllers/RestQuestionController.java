9a6bd23d994c7626165563f091d7edf288ba5800
dangnh-dev <haidang14727@gmail.com>
2022-05-12 11:20:15 +0700

    find question by exam

--- src/main/java/com/example/todoapi/controllers/RestQuestionController.java
+++ src/main/java/com/example/todoapi/controllers/RestQuestionController.java
@@ -1,10 +1,18 @@
 package com.example.todoapi.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.todoapi.services.ServiceImpl.QuestionServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api/question")
 @RestController
 public class RestQuestionController {
+    @Autowired
+    QuestionServiceImpl questionServiceImpl;
 
+    @GetMapping
+    public ResponseEntity<?> getQuestionByExam(@RequestParam("examId")Long examId){
+        return ResponseEntity.ok(questionServiceImpl.getQuestionByExamId(examId));
+    }
 }
--- src/main/java/com/example/todoapi/repositories/QuestionRepository.java
+++ src/main/java/com/example/todoapi/repositories/QuestionRepository.java
@@ -5,6 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Repository
 public interface QuestionRepository extends JpaRepository<QuestionEntity, Long> {
+    List<QuestionEntity> findAllByExamId(Long id);
 }
