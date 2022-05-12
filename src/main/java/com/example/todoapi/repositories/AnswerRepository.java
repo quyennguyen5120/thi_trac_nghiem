0866ed3ed958c96c9c911797a27b6a4e512dec45
vuivehieu <75554300+vuivehieu@users.noreply.github.com>
2022-05-12 11:19:45 +0700

    resultAPI

--- src/main/java/com/example/todoapi/repositories/AnswerRepository.java
+++ src/main/java/com/example/todoapi/repositories/AnswerRepository.java
@@ -2,7 +2,9 @@ package com.example.todoapi.repositories;
 
 import com.example.todoapi.entities.AnswerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AnswerRepository extends JpaRepository<AnswerEntity, Long> {
 
 }
