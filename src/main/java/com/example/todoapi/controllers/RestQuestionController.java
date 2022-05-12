1fadeeb4ee40d2096e6be62e43ad7796297930a1
quyen <nguyenquyen5120@gmail.com>
2022-05-12 09:07:39 +0700

    config base

---
+++ src/main/java/com/example/todoapi/controllers/RestQuestionController.java
@@ -0,0 +1,9 @@
+package com.example.todoapi.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/category")
+@RestController
+public class RestQuestionController {
+}
