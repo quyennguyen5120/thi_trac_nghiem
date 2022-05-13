5d20390eb4c85334da02f06688de4352933df318
dangnh-dev <haidang14727@gmail.com>
2022-05-13 08:32:41 +0700

    images

--- src/main/java/com/example/todoapi/config/ConfigMapping.java
+++ src/main/java/com/example/todoapi/config/ConfigMapping.java
@@ -1,14 +1,17 @@
 package com.example.todoapi.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class ConfigMapping implements WebMvcConfigurer {
+    @Value("${config.upload}")
+    private String directory;
 
-//    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler("/resources/**").addResourceLocations("classpath:/resources/images/");
-//    }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("images/**").addResourceLocations("file:/"+directory);
+    }
 }
